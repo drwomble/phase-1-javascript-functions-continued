87ec5dd8606cb9cc671317665a3e9cf451ac1bc3
Drew <drwomble27@gmail.com>
2023-04-12 11:32:37 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// code your solution here
+
+function saturdayFun(activity){
+    if (activity === undefined){
+    return "This Saturday, I want to roller-skate!";
+}
+    else {
+        return `This Saturday, I want to ${activity}!`;
+    }
+}
+
+function mondayWork(activity){
+    if (activity === undefined){
+        return "This Monday, I will go to the office.";
+    }
+    else {
+        return `This Monday, I will ${activity}.`;
+    }
+}
+
+// function outer(greeting, msg = "It's a fine day to learn") {
+//     const innerFunction = function (name, lang = "Python") {
+//       return `${greeting}, ${name}! ${msg} ${lang}`;
+//     };
+//     return innerFunction;
+//   }
+//   outer("Hello")("student", "JavaScript");
+//=> "Hello, student! It's a fine day to learn JavaScript"
+
+function wrapAdjective(flair = "*"){
+    const encouragingPromptFunction = function (empathy = "special"){
+        return `You are ${flair}${empathy}${flair}!`;
+    }
+    return encouragingPromptFunction;
+}
